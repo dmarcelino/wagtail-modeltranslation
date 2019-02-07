ba634dbca6aefd6946b2a781f7591366f539551c
Dario Marcelino <dario@appscot.com>
2019-02-07 11:14:18 +0000

    adds languages_loop function

--- wagtail_modeltranslation/contextlib.py
+++ wagtail_modeltranslation/contextlib.py
@@ -1,4 +1,4 @@
-from django.utils.translation import activate
+from django.utils.translation.trans_real import activate
 from modeltranslation.utils import get_language
 
 
@@ -17,5 +17,26 @@ class use_language:
     def __enter__(self):
         activate(self.language)
 
-    def __exit__(self, exctype, excinst, exctb):
+    def __exit__(self, exc_type, exc, exc_tb):
+        activate(self.current_language)
+
+
+class revert_to_current_language:
+    """
+    Context manager to safely change languages momentarily and return to current one
+
+    Usage:
+        with revert_to_current_language():
+            activate('pt')
+            pt_url = obj.get_absolute_url()
+            activate('fr')
+            fr_url = obj.get_absolute_url()
+    """
+    def __init__(self):
+        self.current_language = get_language()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc, exc_tb):
         activate(self.current_language)
